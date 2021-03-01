29eec840579c2a25a2c9e44d95653dd069aaf5d8
Marco <marcosinisa.mihajlovic@gmail.com>
2021-03-01 16:32:38 +0100

    Esercizio completato

--- js/main.js
+++ js/main.js
@@ -1,112 +1,112 @@
 var arrayMine = [];
 var arrayUser = [];
-var trovato = true;
 var continua = true;
 var giusto = true;
 var partita = true;
-var partitaConclusa = false;
+var partitaConclusa = true;
 var contatore = 0;
 var sentinella = 0;
 var scelta = "";
 
-while(trovato){
-    for (var i = 0; i < 16; i++) {
-        arrayMine[i] = Math.floor(Math.random()*100 +1);
-    }
-
-    for (var i = 0; i < arrayMine.length - 1; i++) {
-        for (var j = i + 1; j < arrayMine.length; j++) {
-            if (arrayMine[i] == arrayMine[j]) {
-                contatore++;
-            }
-        }
-    }
-
-    if (contatore == 0) {
-        trovato = false;
-    }
-}
-
-console.log(arrayMine);
-contatore = 0;
-
-while(partita) {
-    partitaConclusa = false;
-    continua = true;
-    while(continua) {
-        giusto = true
-        while(giusto) {
-            var numUtente = parseInt(prompt("Inserisci un numero"));
-            if (isNaN(numUtente) || numUtente < 1) {
-                alert("Devi inserire un valore corretto");
-            } else {
-                giusto = false;
-            }
-
-        }
-        
-        if (arrayUser.length > 0) {
-            for (var i = 0; i < arrayUser.length - 1; i++) {
-                for (var j = i + 1; j < arrayUser.length; j++) {
-                    if (arrayUser[i] == arrayUser[j]) {
-                        contatore++;
-                        alert("hai già inserito " + numUtente);
-
-                    }
-                }
-            }
-        }
-        
-        if(contatore == 0) {
-            continua = false;
-        }
-        arrayUser.push(numUtente);
-        
-        contatore = 0;
-    }
+function startGame() {
     
-    contatore = 0;
+    while (partita) {
+        
+        if (partitaConclusa) {
+            inizializeMine();
 
-    var fortuna = campoMinato(numUtente, arrayMine);
+            console.log(arrayMine.sort((a,b) => a-b));
+        }
 
-    if (!fortuna) {
-        alert("Che peccato hai calpestato una mina ed hai fatto " + contatore + " punti");
-        partitaConclusa = true;
-    } else {
-        contatore++;
-        sentinella++;
-    }
 
-    if(contatore == 84) {
-        alert("Complimenti hai vinto, non hai calpestato nessuna mina, hai fatto punteggio massimo");
-        partitaConclusa == true;
-    }
+        partitaConclusa = false;
+        continua = true;
 
-    if (partitaConclusa) {
-        var risposta = true
-        while(risposta){
-            scelta = prompt("Vuoi fare un'altra partita? Y/N").toLowerCase();
-            if(scelta != "y" && scelta != "n"){
-                alert("Inserisci la risposta corretta");
-            } else {
-                risposta = false;
+
+
+        giusto = true
+
+        while (giusto) {
+            giusto = askNumber();
+            console.log(arrayUser);
+        }
+
+        var fortuna = campoMinato(arrayUser[arrayUser.length-1], arrayMine);
+
+        if (fortuna) {
+            alert("Che peccato hai calpestato una mina ed hai fatto " + contatore + " punti");
+            partitaConclusa = true;
+        } else {
+            contatore++;
+            sentinella++;
+        }
+
+        if (contatore == 84) {
+            alert("Complimenti hai vinto, non hai calpestato nessuna mina, hai fatto punteggio massimo");
+            partitaConclusa == true;
+        }
+
+        if (partitaConclusa) {
+            var risposta = true
+            while (risposta) {
+                scelta = prompt("Vuoi fare un'altra partita? Y/N").toLowerCase();
+                if (scelta != "y" && scelta != "n") {
+                    alert("Inserisci la risposta corretta");
+                } else {
+                    risposta = false;
+                }
+
+            }
+
+            if (scelta == "n") {
+                partita = false;
             }
 
         }
 
-        if (scelta == "n") {
-            partita = false;
-        }
-
     }
-
 }
+
 
 function campoMinato(utente, mine) {
     for (var i = 0; i < mine.length; i++) {
-        if (utente == mine[i]){
-            return false;
+        if (utente == mine[i]) {
+            return true;
         }
     }
+    return false;
+}
+
+function inizializeMine() {
+    arrayMine = [];
+    arrayUser = [];
+    contatore = 0;
+    for (var i = 0; i < 16; i++) {
+        var inserito = false;
+
+        while (!inserito) {
+            var numero = Math.floor(Math.random() * 100 + 1);
+            if (!arrayMine.includes(numero)) {
+                arrayMine.push(numero);
+                inserito = true;
+            }
+        }
+
+    }
+}
+
+function askNumber() {
+    var numUtente = parseInt(prompt("Inserisci un numero"));
+    if (isNaN(numUtente) || numUtente < 1 || numUtente > 100) {
+        alert("Devi inserire un valore corretto");
+    } else {
+        if (!arrayUser.includes(numUtente)) {
+            arrayUser.push(numUtente);
+            return false;
+        } else {
+            alert("hai già inserito " + numUtente);
+        }
+    }
+
     return true;
 }
